1949205f4ae3176c89f46784ff3c0933cc83016f
Nguyễn Quang An <ngquangan@gmail.com>
2020-11-26 21:17:59 +0700

    Generate token when login

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './enities/user.entity';
 import { UserService } from './users.service';
 import { UserResolver } from './users.resolver';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([User])],
+    imports: [TypeOrmModule.forFeature([User]), ConfigService],
     providers: [UserResolver, UserService]
 })
 export class UserModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,13 +1,19 @@
 import { InjectRepository } from '@nestjs/typeorm';
 import { Injectable } from "@nestjs/common";
 import { Repository } from 'typeorm';
+import * as jwt from 'jsonwebtoken';
+
 import { User } from './enities/user.entity';
 import { CreateAccountInput } from './dtos/create-account.dto';
 import { LoginInput, LoginOutput } from './dtos/login.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class UserService {
-    constructor(@InjectRepository(User) private readonly users: Repository<User>) { }
+    constructor(
+        @InjectRepository(User) private readonly users: Repository<User>,
+        private readonly config: ConfigService
+        ) { }
 
     async createAccount({ email, password, role }: CreateAccountInput): Promise<{ ok: boolean, error?: string }> {
         try {
@@ -41,9 +47,12 @@ export class UserService {
                 throw new Error('Wrong password!');
             }
 
+            const token = jwt.sign({ id: user.id }, this.config.get('SECRET_KEY'));
+
             return {
                 ok: true,
                 error: null,
+                token
             }
 
         } catch (e) {
